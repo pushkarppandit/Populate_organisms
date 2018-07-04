ff0c9599f25ca16bf14d8c36fef0f53debdb001e
Pushkar <pushkar.p.pandit@gmail.com>
2018-07-04 23:29:34 +0530

    experiments

--- populate.py
+++ populate.py
@@ -15,37 +15,37 @@ class organism:
         self.pop_list = []
 
 
-    def pop_update(self,org_list):
+    def pop_update(self,org_list,m_prev):
         if self.level>=1:
             food = org_list[self.level-1]
             food_pop = np.array([org.pop for org in food])
             food_m = np.array([org.m for org in food])
-            food_mass = np.dot(food_m,food_pop)+ 0.0001
+            food_mass = np.dot(food_m,food_pop)+ 0.0000001
         else:
-            food_mass=0.1
-            for key in org_list:
-                food_mass += 0.05*sum([o.m for o in org_list[key]])
+            food_mass=m_prev
         if self.level<len(org_list)-1:
             pred = org_list[self.level+1]
             pred_pop = np.array([org.pop for org in pred])
             pred_ap = np.array([org.ap for org in pred])
-            pred_eat = np.dot(pred_pop,pred_ap)+ 0.0001
+            pred_eat = np.dot(pred_pop,pred_ap)+ 0.0000001
         else:
             pred_eat=0
         level = org_list[self.level]
         level_pop = np.array([org.pop for org in level])
         level_def = np.array([org.defence for org in level])
         level_agg = np.array([org.agg for org in level])
-        level_def_t = np.dot(level_pop,level_def) + 0.0001
-        level_agg_t = np.dot(level_pop, level_agg) + 0.0001
+        level_def_t = np.dot(level_pop,level_def) + 0.0000001
+        level_agg_t = np.dot(level_pop, level_agg) + 0.0000001
         # level_pop_t = level_pop.sum()
 
         self.pop_list.append(self.pop)
 
-        dp = ((self.vi*self.pop - (self.defence*self.pop/level_def_t)*pred_eat) + ((self.agg*self.pop/level_agg_t)*food_mass - self.ap*self.pop)) - self.death
-        pop_new = self.pop + 0.01*dp
+        dp = ((self.vi*self.pop - (self.defence*self.pop/level_def_t)*pred_eat) + ((self.agg*self.pop/level_agg_t)*food_mass - self.ap*self.pop)) - 1*self.death
+        print(dp/self.pop)
+        pop_new = self.pop + 0.005*dp
         pop_new = np.max([pop_new,0])
         return pop_new
+
 
     def disaster(self,maxi=0.5):
         # self.pop_list.append(self.pop)
@@ -66,8 +66,17 @@ class ecosystem:
     def simulate(self,iter,dis_prob):
         for i in range(iter):
             pop_dict = {}
+            if i>0:
+                m_prev=m_now
+            m_now = 0
             for key in self.org_dict:
-                pop_dict[key] = [org_l.pop_update(self.org_dict) for org_l in self.org_dict[key]]
+                food_pop = np.array([org.pop for org in self.org_dict[key]])
+                food_m = np.array([org.m for org in self.org_dict[key]])
+                m_now += 0.18 * np.dot(food_m, food_pop) + 0.0000001
+            if i==0:
+                m_prev=m_now
+            for key in self.org_dict:
+                pop_dict[key] = [org_l.pop_update(self.org_dict,m_prev) for org_l in self.org_dict[key]]
             for key in self.org_dict:
                 for i,org_l in enumerate(self.org_dict[key]):
                     if (key==0) and (np.random.rand() < dis_prob):
@@ -90,20 +99,21 @@ class ecosystem:
 
 # organism: [pop,vi,ap,defence,agg,m,death]
 config_dict1 = {
-    0: np.array([[10,1.3,1,1,1,1,0.1]]),
-    1: np.array([[2,0.9,0.8,8,5,2,0.1],
-                   [2,0.7,0.6,10,7,1,0.1]]),
-    2: np.array([[1,0.1,1.5,1,1,4,0.1]])
-}
-# organism: [pop,vi,ap,defence,agg,m,death]
+    0: np.array([[10,1.3,1  ,1 ,1,2,0.1]]),
+    1: np.array([[2 ,0.9,0.3,8 ,5,2,0.2],
+                 [2 ,0.7,0.2,10,7,1,0.2]]),
+    2: np.array([[1 ,0.1,1.5,1 ,1,4,0.3]])
+}# organism: [pop,vi,ap,defence,agg,m,death]
 config_dict2 = {
-    0: np.array([[7,1.3,1,1,1.1,1,0.2],
-                 [7,1.1,1.2,0.8,2,1,0.1],
-                 [7,1.4,1,0.9,1,1,0.1]]),
-    1: np.array([[2,0.9,0.8,8,5,2,0.1],
-                   [2,0.7,0.6,10,7,1,0.1]]),
-    2: np.array([[1,0.1,1.5,1,1,4,0.05]])
+    0: np.array([[6 ,1.8 ,1  ,1  ,1.1,8  ,0.09],
+                 [5 ,2.4 ,1.2,0.8,2  ,7  ,0.12],
+                 [5 ,2.3 ,1  ,0.9,1.1,12,0.08]]),
+    1: np.array([[3 ,0.8 ,1.8,8  ,5  ,1  ,0.15],
+                 [3 ,0.7 ,1.6,10 ,5  ,1.1,0.15]]),
+    2: np.array([[1 ,0.1 ,2.5,1  ,1  ,1.5  ,0.3 ],
+                 [1, 0.1 ,2.5,1  ,1.3,1.8  ,0.3 ]])
 }
+
 eco1 = ecosystem(config_dict2)
 eco1.simulate(2000,0)
 eco1.plot_pop()
